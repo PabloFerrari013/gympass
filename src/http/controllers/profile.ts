7d73650affd9159baffb607f7d6e93510c3a360c
Pablo Ferrari <pabloferraricaliari@gmail.com>
2023-04-13 13:44:08 -0300

    problem with esbuild

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,8 +1,17 @@
 import { InvalidCredentialsError } from "./../../use-cases/erros/invalid-credentials-error";
 import { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
-import { makeAuthenticateUseCase } from "@/use-cases/factory/make-authenticate-use-case";
+import { makeGetUserProfileUseCase } from "@/use-cases/factory/make-get-user-profile-use-case";
 
 export async function profile(req: FastifyRequest, res: FastifyReply) {
-  return res.status(200).send();
+  const getUserProfile = makeGetUserProfileUseCase();
+
+  const { user } = await getUserProfile.execute({
+    userId: req.user.sub,
+  });
+  return res.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  });
 }
